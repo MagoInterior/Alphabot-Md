844220e54d305efdb2fca52e58e74aa8d88afd37
Ova Botz <110052468+Hitomimd@users.noreply.github.com>
2023-03-06 00:20:29 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ const {
    spanyol
 } = require(`./language`)
 
-global.OpenAIKey = "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" //login to https://platform.openai.com/account/api-keys to get apikey
+global.OpenAIKey = "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" //login to https://platform.openai.com/account/api-keys to get apikey
 // Website Api (jgn di ganti tomlol)
 global.APIs = {
    alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
